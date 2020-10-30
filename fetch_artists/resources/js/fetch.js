76f10e49188de6ec294a49f49b35ac1546b9f143
quamepossible <appiahkwame274@gmail.com>
2020-10-30 13:30:58 +0000

    allImages variable declared outside ajax scope

--- fetch_artists/resources/js/fetch.js
+++ fetch_artists/resources/js/fetch.js
@@ -4,13 +4,10 @@ $('.myForm').submit((e)=>{
 
 $('.input').keyup(() => {
     var user = $('.input').val();
-    console.log(user)
-    // var parImg = document.querySelector('.holimg');
-    var allImgs = document.querySelector('img');
+    var allImages = document.querySelector('img');
 
     $('.holdata').html('');
     $('.status').html('');
-    // $('.yoimg').attr('src', '');
     $('.input').css({
         border: "3px solid black",
         boxShadow : "none"        
@@ -22,7 +19,7 @@ $('.input').keyup(() => {
         data : {name : user},
         success : (data) => {
             var artDat = data;
-            console.log(artDat);
+            // console.log(artDat);
             if(artDat == 'empty'){
                 $('.status').html('Artist not found');
                 $('.input').css({
@@ -40,12 +37,12 @@ $('.input').keyup(() => {
                 var count = 0;
                 for(i = 0; i < artDat.length; i++){
                     var dezName = artDat[i].stage;
-                    console.log(dezName);
+                    // console.log(dezName);
 
                     //create a div element to hold individual record
                     var newDiv = document.createElement('div');
 
-                    //four spans
+                    //create five spans
                     var imgSpan = document.createElement('span');
                     var nameSpan = document.createElement('span');
                     var stageSpan = document.createElement('span');
@@ -54,7 +51,6 @@ $('.input').keyup(() => {
 
                     //add class to imgSpan
                     imgSpan.classList.add('picspan');
-
 
                     //add class to viewSpan
                     viewSpan.classList.add('view');
@@ -94,8 +90,7 @@ $('.input').keyup(() => {
 
                     $('.input').css({
                         border: "3px solid green",
-                        boxShadow : "none"
-                        
+                        boxShadow : "none",                        
                     })
 
                     $.ajax({
@@ -107,13 +102,13 @@ $('.input').keyup(() => {
                             var dezData = data;   
                             newSrc = dezData.picture_xl;
                             imgArr[count] = newSrc;
-                            allImages = document.querySelectorAll('img');
                             for(j = 0; j < artDat.length; j++){
-                                allImages[j].setAttribute('src', imgArr[j]);
-                                allImages[j].classList.add('yoimg');
+                                if(allImages[j] != undefined){
+                                    allImages[j].setAttribute('src', imgArr[j]);
+                                    allImages[j].classList.add('yoimg');
+                                }
                             }
                             count++;
-                            console.log(data);
                         }
                     })
                 }
